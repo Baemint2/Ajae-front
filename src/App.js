783250afd5f601e66271852fad413cfb9872e6f9
Baemint2 <qofndlwl@gmail.com>
2024-11-01 01:15:34 +0900

    기초 세팅

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-2xl font-bold underline">
+          오늘의 개-그
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
