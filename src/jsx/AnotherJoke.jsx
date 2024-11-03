e2d546b6b641c3c26e93b4f046322f39737cae10
Baemint2 <qofndlwl@gmail.com>
2024-11-03 23:56:34 +0900

    [feat] anotherJoke -> userJoke로 변경

--- src/jsx/AnotherJoke.jsx
+++
@@ -1,9 +0,0 @@
-const AnotherJoke = () => {
-    return (
-        <div>
-            <h1 class="text-3xl font-bold">유저들의 개-그</h1>
-        </div>
-    )
-}
-
-export default AnotherJoke;
---
+++ src/jsx/UserJoke.jsx
@@ -0,0 +1,51 @@
+import React, {useState,  useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
+
+import "../css/userJoke.css";
+
+const UserJoke = () => {
+    const navigate = useNavigate();
+
+    const [userJoke, setUserJoke] = useState([]);
+
+    useEffect(() => {
+        getAllUserJoke();
+    }, []);
+
+    const getAllUserJoke = () => {
+        fetch("/api/v1/allUserJoke")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setUserJoke(data);
+            })
+    }
+
+    const jokeClick = (jokeId, userId) => {
+       navigate(`/userJoke/${jokeId}`);
+       localStorage.setItem("userId", JSON.stringify(userId));
+    }
+
+    return (
+        <div className="another-joke">
+            <h1 className="text-3xl font-bold">유저들의 개-그</h1>
+            {userJoke.map((item, index) => (
+            <div className="border border-sky-400 rounded-3xl mb-10"  key={index}
+                 onClick={() => jokeClick(item.joke.id, item.user.id)}>
+                <div className="user-wrap">
+                    <img src={item.user.profile} alt="유저 프로필 사진" className="mx-5 my-5"></img>
+                    <span>{item.user.nickname}</span>
+                </div>
+                <div className="question-wrap">
+                    <span className="question">{item.joke.question}</span>
+                </div>
+                <div className="temp">
+                    조회수 좋아요
+                </div>
+            </div>
+            ))}
+        </div>
+    )
+}
+
+export default UserJoke;
