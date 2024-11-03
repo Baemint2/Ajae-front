1463848dfcb7ec48a6779e6ab9a338449ebbad89
Baemint2 <qofndlwl@gmail.com>
2024-11-03 23:56:05 +0900

    [fix] 스타일 수정, 이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './css/App.css';
+import {useNavigate} from "react-router-dom";
 
 function App() {
+    const navigate = useNavigate();
+
+    const goToJoke = () => {
+        navigate("/joke");
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-      <a href="/jsx/Joke">오늘의 개-그</a>
+      <h1 onClick={goToJoke} >오늘의 개-그</h1>
       </header>
     </div>
   );
